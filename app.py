9e60baa956ccfdac7497f38c4e0317114bb0a4c0
KotR9001 <57807780+KotR9001@users.noreply.github.com>
2020-03-30 13:07:57 -0500

    Make changes to app.py.

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
-###Climate App
-
-#Import Flask & climate_starter notebook
-#Method to Allow Import of Python Files Found at https://stackoverflow.com/questions/4142151/how-to-import-the-class-within-the-same-directory-or-sub-directory
-from flask import Flask, jsonify
-
-#Create App
-app = Flask(__name__)
-
-#Create the Precipitation List
-prcp_list = []
-
-#Create the Stations List
-stations_list = []
-
-#Create the Temperature List for the Most Active Station from the Last Year
-temp_list = []
+#########Climate App
 
 #####Re-Enter Code From Jupyter Notebook
 
@@ -152,6 +136,22 @@ temp_df['Date'] = pd.DataFrame(temp_df['Date'].tolist())
 temp_df['Temperature'] = pd.DataFrame(temp_df['Temperature'].tolist())
 
 ###Create New Code for the Climate App
+#Import Flask & climate_starter notebook
+#Method to Allow Import of Python Files Found at https://stackoverflow.com/questions/4142151/how-to-import-the-class-within-the-same-directory-or-sub-directory
+from flask import Flask, jsonify, Response
+
+#Create App
+app = Flask(__name__)
+
+#Create the Precipitation List
+prcp_list = []
+
+#Create the Stations List
+stations_list = []
+
+#Create the Temperature List for the Most Active Station from the Last Year
+temp_list = []
+
 #Define the Start & End Dates
 start = 3/18/2012
 end = 3/18/2017
@@ -175,52 +175,61 @@ print(f"The average temperature at the most active station was {avg_temp2}oC.")
 #Create the Homepage
 app.route("/")
 def home():
-    print("Here is the homepage.")
-    print("---------------------")
-    print("Here is the directory of routes.")
-    print("--------------------------------")
-    print("/api/v1.0/precipitation")
-    print("/api/v1.0/stations")
-    print("/api/v1.0/tobs")
-    print("/api/v1.0/<start>")
-    print("/api/v1.0/<start>/<end>")
+    return(
+        f"Here is the homepage</br>."
+        f"---------------------------------</br>"
+        f"Here is the directory of routes</br>."
+        f"-----------------------------------------</br>"
+        f"Here is the page with precipitation data</br>."
+        f"/api/v1.0/precipitation</br>"
+        f"----------------------------------------</br>"
+        f"Here is the page with the stations list</br>." 
+        f"/api/v1.0/stations</br>"
+        f"-----------------------------------------------------------------------------------------------</br>"
+        f"Here is the page with the temperature data from the most active station from the previous year</br>."
+        f"/api/v1.0/tobs</br>"
+        f"------------------------------------------------------------------------------------------------------------------------------</br>"
+        f"Here is the list of minimum, average, and maximum temperature values from the specified start date to the last available date</br>."
+        f"/api/v1.0/<start></br>"
+        f"------------------------------------------------------------------------------------------------------------------------------</br>"
+        f"Here is the list of minimum, average, and maximum temperature values from the specified start date to the specified end date</br>."
+        f"/api/v1.0/<start>/<end>"
+    )
 
 #Create the Precipitation Page
 app.route("/api/v1.0/precipitation")
 def precipitation():
-    print("Here is the page with precipitation data.")
     for date, prcp in session.query(Measurement.date, Measurement.prcp):
         prcp_dict = {}
         prcp_dict["date"] = date
         prcp_dict["prcp"] = prcp
         prcp_list.append(prcp_dict)
     return jsonify(prcp_list)
+        
 
 #Create the Stations Page
 app.route("/api/v1.0/stations")
 def station():
-    print("Here is the page with the stations list.")
     for station in session.query(Measurement.station):
         stations_dict = {}
         stations_dict["station"] = station
         stations_list.append(stations_dict)
     return jsonify(stations_list)
+    
 
 #Create the Temperature Page for the Most Active Station from the Last Year
 app.route("/api/v1.0/tobs")
 def tobs():
-    print("Here is the page with the temperature data from the most active station from the previous year.")
     for date, tobs in session.query(Measurement.date, Measurement.tobs):
         temp_dict = {}
         temp_dict["date"] = date
         temp_dict["tobs"] = tobs
         temp_list.append(temp_dict)
-    return jsonify(temp_dict)
+    return jsonify(temp_list)
 
 #Create the List of Minimum, Average, and Maximum Temperature Values from the Specified Start Date Where the End Date is Not Specified
 app.route("/api/v1.0/<start>")
-def extremes1():
-    print("Here is the list of minimum, average, and maximum temperature values from the specified start date to the last available date.")
+def extremes1():    
     return jsonify([f"The minimum temperature in the date range is: {low_temp1}oF.",
                     f"The average temperature in the date range is: {avg_temp1}oF.",
                     f"The maximum temperature in the date range is: {high_temp1}oF."])
@@ -228,7 +237,6 @@ def extremes1():
 #Create the List of Minimum, Average, and Maximum Temperature Values from the Specified Start Date to the Specified End Date
 app.route("/api/v1.0/<start>/<end>")
 def extremes2():
-    print("Here is the list of minimum, average, and maximum temperature values from the specified start date to the specified end date.")
     return jsonify([f"The minimum temperature in the date range is: {low_temp2}oF.",
                     f"The average temperature in the date range is: {avg_temp2}oF.",
                     f"The maximum temperature in the date range is: {high_temp2}oF."])
